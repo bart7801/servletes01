5ffa5791a66f7c3f5fce040331e3f8cfb58ac30e
bart7801 <bart7801@o2.pl>
2019-09-15 13:20:17 +0100

    RandomJson2

--- src/main/java/pl/sda/first/RandomJson.java
+++ src/main/java/pl/sda/first/RandomJson.java
@@ -10,10 +10,21 @@ import java.util.Random;
 
 @WebServlet(name = "RandomJson",
         urlPatterns = {"/getRandomNumber", "/randomJson"})
-
 public class RandomJson extends HttpServlet {
+
+    private static final String PARAMETR_NAME = "giveRandomNumbers";
+    private static final String EXPECTED_VALUE = "true";
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws IOException {
+        String giveRandomNumbers = request.getParameter(PARAMETR_NAME);
+        if (EXPECTED_VALUE.equals(giveRandomNumbers)) {
+            //TODO
+        } else {
+            System.out.printf("Hello");//print hello on web
+        }
+
         PrintWriter writer = response.getWriter();
         response.setContentType("application/json");
         Random random = new Random();
